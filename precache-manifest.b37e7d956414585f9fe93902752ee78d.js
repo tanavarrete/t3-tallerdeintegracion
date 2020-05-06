3224656a1ac502fa63c0ad57f40de3861c511815
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-05-06 10:11:11 -0400

    Updates

---
+++ precache-manifest.b37e7d956414585f9fe93902752ee78d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf03b6431a57a237857b3c30f9800d6",
+    "url": "/t3-tallerdeintegracion/index.html"
+  },
+  {
+    "revision": "ef6c2f93cf8b73442fb6",
+    "url": "/t3-tallerdeintegracion/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aacbcb373fc4c0cd4054",
+    "url": "/t3-tallerdeintegracion/static/js/2.7564da71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/t3-tallerdeintegracion/static/js/2.7564da71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6c2f93cf8b73442fb6",
+    "url": "/t3-tallerdeintegracion/static/js/main.b8fcd6d3.chunk.js"
+  },
+  {
+    "revision": "2ee6e56224e6ac0f0e11",
+    "url": "/t3-tallerdeintegracion/static/js/runtime-main.799f76fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/t3-tallerdeintegracion/static/media/logo.5d5d9eef.svg"
+  }
+]);
